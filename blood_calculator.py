0a77e5091132889176a52d7a266bf764f200ac9b
Vanessa Tam <vanessatam@Vanessas-MacBook-Pro-2.local>
2023-01-20 12:45:20 -0500

    Classify HDL result in qualitative terms

--- blood_calculator.py
+++ blood_calculator.py
@@ -15,4 +15,14 @@ def HDL_input():
 	HDL_value = int(HDL_value)
 	return HDL_value
 
+
+def HDL_analysis(HDL_int):
+	if HDL_int >= 60:
+		answer = "Normal"
+	elif 40 <= HDL_int < 60:
+		answer = "Borderline Low"
+	else:
+		answer = "Low"
+	return answer 
+
 interface()
