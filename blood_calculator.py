2822b88813bfa6c196eb1527fdaa7c4bb831af52
Vanessa Tam <vanessatam@Vanessas-MacBook-Pro-2.local>
2023-01-20 13:07:29 -0500

    LDL results are analyzed and outputted to the console

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,12 +4,15 @@ def interface():
 	while keep_running: 
 		print("Options:")
 		print("1 - HDL")
+		print("2 - LDL")
 		print("9 - Quit")
 		choice = input("Select an option: ")
 		if choice == "9":
 			keep_running = False
 		elif choice == "1":
 			HDL_driver()
+		elif choice == "2":
+			LDL_driver()
 	print("Program ending")
 
 def HDL_driver():
@@ -34,6 +37,32 @@ def HDL_analysis(HDL_int):
 
 def HDL_output(HDL_value, HDL_analy):
 	print("The HDL result of {} is considered {}".format(HDL_value, HDL_analy))
+	return
+
+def LDL_driver():
+	LDL_in = LDL_input()
+	LDL_analy = LDL_analysis(LDL_in)
+	LDL_output(LDL_in, LDL_analy)
+
+def LDL_input():
+	LDL_value = input("Enter the LDL result: ")
+	LDL_value = int(LDL_value)
+	return LDL_value
+
+
+def LDL_analysis(LDL_int):
+	if LDL_int < 130:
+		answer = "Normal"
+	elif 130 <= LDL_int <= 159:
+		answer = "Borderline High"
+	elif 160 <= LDL_int <= 189:
+		answer = "High"
+	else:
+		answer = "Very High"
+	return answer 
+
+def LDL_output(LDL_value, LDL_analy):
+	print("The LDL result of {} is considered {}".format(LDL_value, LDL_analy))
 	return
 
 interface()
