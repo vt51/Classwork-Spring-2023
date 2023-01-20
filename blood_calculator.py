fcaa7e6dee7c59d64e46bf3ddde68a81a0a65905
Vanessa Tam <vanessatam@Vanessas-MacBook-Pro-2.local>
2023-01-20 12:49:03 -0500

    Connected HDL_driver function to the interface

--- blood_calculator.py
+++ blood_calculator.py
@@ -3,12 +3,18 @@ def interface():
 	keep_running = True
 	while keep_running: 
 		print("Options:")
+		print("1 - HDL")
 		print("9 - Quit")
 		choice = input("Select an option: ")
 		if choice == "9":
 			keep_running = False
+		elif choice == "1":
+			HDL_driver()
 	print("Program ending")
 
+def HDL_driver():
+	HDL_in = HDL_input()
+	HDL_analy = HDL_analysis(HDL_input)
 
 def HDL_input():
 	HDL_value = input("Enter the HDL result: ")
