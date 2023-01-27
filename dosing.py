7f91b979c9ac281498c413f977e437000ba2b561
Vanessa Tam <vanessatam@Vanessas-MacBook-Pro-2.local>
2023-01-27 12:54:56 -0500

    Made modular code

--- dosing.py
+++ dosing.py
@@ -5,8 +5,15 @@
              type of medical treatment or diagnostics.
 """
 
-
 def dose_amount():
+    diagnosis_entry = get_user_input_diagnosis()
+    diagnosis = convert_entry_to_int(diagnosis_entry)
+    weight_entry = get_user_input_patient_weight()
+    weight = convert_weight_entry_to_weight_kg(weight_entry)
+    dosage_mg_first_day = calculate_dosage(diagnosis, weight)
+    output_results(weight, dosage_mg_first_day)
+
+def get_user_input_diagnosis():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -14,24 +21,38 @@ def dose_amount():
     print("2 - Acute bacterial sinusitis")
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
-    diagnosis = int(input("Enter a number: "))
+    diagnosis = input("Enter a number: ")
+    return diagnosis
+
+def convert_entry_to_int(entry):
+    return int(entry)
+
+def get_user_input_patient_weight():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
+    return weight_input
+
+def convert_weight_entry_to_weight_kg(weight_input):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    return weight
+
+def calculate_dosage(diagnosis, weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return dosage_mg_first_day
+
+def output_results(weight, dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
-
 
 if __name__ == '__main__':
     dose_amount()
