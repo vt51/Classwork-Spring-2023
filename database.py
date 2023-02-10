6aad29872899ce244a0697bbb108ba5f80b39485
Vanessa Tam <vanessatam@Vanessas-MacBook-Pro-2.local>
2023-02-10 12:40:20 -0500

    Finished conversion of code to using dictionary

--- database.py
+++ database.py
@@ -7,20 +7,34 @@ def create_patient_entry(first_name, last_name, patient_mrn, patient_age):
     return new_patient
 
 
+def get_full_name(patient):
+    return "{} {}".format(patient["First Name"], patient["Last Name"])
+
+
+def print_database(db):
+    for patient in db:
+        print("MRN: {}, Full Name: {}, Age: {}".format(patient["MRN"], 
+                                                       get_full_name(patient), 
+                                                       patient["Age"]))
+
+
 def main_driver():
     db = []
     db.append(create_patient_entry("Ann", "Ables", 1, 34))
     db.append(create_patient_entry("Bob", "Boyles", 2, 45))
     db.append(create_patient_entry("Chris", "Chou", 3, 52))
     print(db)
-    return
+    print_database(db)
     add_test_to_patient(db, 1, "HDL", 120)
     add_test_to_patient(db, 2, "LDL", 100)
     add_test_to_patient(db, 2, "HDL", 99)
-    room_numbers = ["103", "232", "333"]
     print(db)
-    print_directory(db, room_numbers)
+    print_database(db)
+    # room_numbers = ["103", "232", "333"]
+    # print(db)
+    # print_directory(db, room_numbers)
     print(get_test_result(db, 2, "LDL"))
+    return
 
 
 def print_directory(db, room_numbers):
@@ -32,7 +46,7 @@ def print_directory(db, room_numbers):
 
 def get_patient_entry(db, mrn_to_find):
     for patient in db:
-        if patient[1] == mrn_to_find:
+        if patient["MRN"] == mrn_to_find:
             return patient
     return False
 
@@ -42,7 +56,7 @@ def add_test_to_patient(db, mrn_to_find, test_name, test_value):
     if patient is False:
         print("Bad entry")
     else:
-        patient[3].append([test_name, test_value])
+        patient["Tests"].append([test_name, test_value])
     return
 
 
@@ -55,7 +69,7 @@ def get_test_value_from_test_list(test_list, test_name):
 
 def get_test_result(db, mrn, test_name):
     patient = get_patient_entry(db, mrn)
-    test_value = get_test_value_from_test_list(patient[3], test_name)
+    test_value = get_test_value_from_test_list(patient["Tests"], test_name)
     return test_value
 
 
