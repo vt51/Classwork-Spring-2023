67262edd73f461ae4567e1eabdfa6293eaa9a68a
vt51 <vanessa.tam@duke.edu>
2023-03-03 15:04:41 -0500

    Classwork on 3-3-23

---
+++ health_db_server.py
@@ -0,0 +1,10 @@
+'''
+Database = dictionary
+keys -> ids for the patients
+value: dict
+
+{1: {"id": 1, "name": "David", "blood_type": "O+"},
+ 2: {"id": 1, "name": "David", "blood_type": "O+"},
+ 3: {"id": 1, "name": "David", "blood_type": "O+", "tests": []},
+'''
+
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -11,6 +11,39 @@ def server_status():
 @app.route("/info", methods=["GET"])
 def info_route():
     return "This server was written for BME 547"
+
+
+@app.route("/HDL_analysis", methods=["POST"])
+def HDL_route_handler():
+    """
+    in_data = {"name": <patient_name>
+               "HDL_value": <HDL_result>}
+    """
+    from blood_calculator import HDL_analysis
+    in_data = request.get_json()
+    print("Received HDL value of {}". format(in_data["HDL_value"]))
+    diagnosis = HDL_analysis(in_data["HDL_value"])
+    return diagnosis
+
+
+@app.route("/add", methods=["POST"])
+def add_numbers():
+    """
+    in_data = {"a": <number>
+               "b": <number>}
+    """
+    in_data = request.get_json()
+    answer = in_data["a"] + in_data["b"]
+    if answer < 0:
+        return "The answer was less than zero. BAD", 400
+    return jsonify(answer)
+
+
+@app.route("/add_two/<a>/<b>", methods=["GET"])
+def add_two_handlers(a, b):
+    answer = int(a) + int(b)
+    return jsonify(answer)
+
 
 
 if __name__ == '__main__':
